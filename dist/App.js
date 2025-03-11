4154fc967a5cd1cc916aed16a6d1a2a6c0ca24b8
Jerome-Spirinova <jerome@spirinova.dev>
2025-03-12 00:02:34 +0530

    dist folder included

---
+++ dist/App.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+// import { CustomBtn } from './components/CustomBtn'
+
+function App() {
+  return /*#__PURE__*/React.createElement(React.Fragment, null);
+}
+export default App;
---
+++ dist/components/CustomBtn.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export const CustomBtn = () => {
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => alert('Button clicked!')
+  }, "Custom Button");
+};
